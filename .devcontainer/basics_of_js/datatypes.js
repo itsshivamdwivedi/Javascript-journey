5818dff2af9317050325b465de8e9897d4e6705c
itsshivamdwivedi <88644787+itsshivamdwivedi@users.noreply.github.com>
2024-05-26 21:00:34 +0000

    datatypes

---
+++ .devcontainer/basics_of_js/datatypes.js
@@ -0,0 +1,19 @@
+"use strict";  // it treats all the js code as the newer version 
+
+// alert(3 + 3)
+console.log(3-3)
+
+let name = " shivam"
+let age = 12
+let isavailable = false
+//  number => 2 to power 
+// bigint for very large numbers 
+//  string = ""
+//  boolean means true or false
+//  null is actually a standalone value 
+// undefined means the values are not defined 
+// symbol is used for uniquenes
+
+
+//  object 
+console.log(typeof 23)
---
+++ .devcontainer/basics_of_js/variables.js
@@ -0,0 +1,13 @@
+const accountID = 14453
+let accountemail = "xyz@gmail.com"
+var accountemailpass = "12345"
+accountcity = "jaipur"
+
+accountemail = "iohfiha@gmail.com"
+accountcity = "kanpur"
+accountemailpass = "oiwhfh"
+// accountID = 2 not allowed 
+
+console.log(accountID);
+console.table([accountID, accountemail, accountemailpass, accountcity])
+ 
