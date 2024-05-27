495fa645ddd5be0b9e942547a31a2c511ea3c67f
itsshivamdwivedi <88644787+itsshivamdwivedi@users.noreply.github.com>
2024-05-27 15:59:36 +0000

    datatypes summary

--- .devcontainer/basics_of_js/conversions&Operations.js
+++ .devcontainer/basics_of_js/conversions&Operations.js
@@ -1,3 +1,14 @@
 "use strict";
 let score = 33
-console.log(typeof score)
+// console.log(typeof score)
+
+// let valueInnumber = String(score)
+// console.log(valueInnumber)
+// console.log(typeof valueInnumber)
+
+console.log("1" + 2)
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+console.log( (3+4) * 5 % 3)
+
+
